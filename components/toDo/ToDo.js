cc8758e531dba77d2cc8af9aae1012a3a95a280d
Nargizazhumabekova <Zhumabekova@amanatcredit.kg>
2024-02-16 22:24:51 +0600

    Added 2.js

---
+++ components/toDo/ToDo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import classes from './ToDo.module.css';
+import { Button } from '../button/Button';
+
+
+/*const ToDo = ({task, handleDelete}) => {
+    return (
+        <li className={classes.task}>
+            <p>id: {task.id}</p>
+            <p>title: {task.title}</p>
+            <Button text={'delete'} onClick={()=>handleDelete(task.id)}/>
+        </li>
+    );
+};
+
+export default ToDo;*/
+//дз
+const ToDo = ({ task, todos, setTodos }) => {
+  const handleDelete = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <li className={classes.task}>
+      <p>id: {task.id}</p>
+      <p>title: {task.title}</p>
+      <Button text={'delete'} onClick={() => handleDelete(task.id)} />
+    </li>
+  );
+};
+
+export default ToDo;
